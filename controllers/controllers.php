89ba62948044474ea618bcc8e32ff82e036bca64
Ben Werdmuller <ben@benwerd.com>
2014-06-29 12:43:52 -0700

    Settings boilerplate

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -1,16 +1,30 @@
 <?php
 
-$app->get('/', function($format='html') use($app) {
-  $res = $app->response();
+    $app->get('/', function ($format = 'html') use ($app) {
+        $res = $app->response();
 
+        ob_start();
+        render('index', array(
+            'title'       => 'IndieReader',
+            'meta'        => '',
+            'authorizing' => false
+        ));
+        $html = ob_get_clean();
+        $res->body($html);
+    });
 
-  ob_start();
-  render('index', array(
-    'title' => 'IndieReader',
-    'meta' => '',
-    'authorizing' => false
-  ));
-  $html = ob_get_clean();
-  $res->body($html);
-});
+    $app->get('/settings/?', function($format = 'html') use ($app) {
+
+        $res = $app->response();
+
+        ob_start();
+        render('settings', array(
+            'title'       => 'Settings',
+            'meta'        => '',
+            'authorizing' => false
+        ));
+        $html = ob_get_clean();
+        $res->body($html);
+
+    });
 
