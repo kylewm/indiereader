8164061cc5b918a6aeacd732191133b9be3ecfd9
Aaron Parecki <aaron@parecki.com>
2014-06-29 13:06:07 -0700

    add sample subscription to the docs page

--- views/docs.php
+++ views/docs.php
@@ -5,6 +5,28 @@
   <h2>Creating the Subscription List</h2>
 
 
-
+  <pre><?= htmlspecialchars(<<<'EOT'
+<div class="h-feed">
+  <h2 class="p-name">Subscription</h2>
+  <ul>
+    <li class="h-entry">
+      <a href="http://aaronparecki.com/" class="u-feed">Aaron Parecki</a>
+    </li>
+    <li class="h-entry">
+      <a href="http://werd.io/" class="u-feed">Ben Werdmüller</a>
+    </li>
+    <li class="h-entry">
+      <a href="http://waterpigs.co.uk/" class="u-feed">Barnaby Walters</a>
+    </li>
+    <li class="h-entry">
+      <a href="http://tantek.com/" class="u-feed">Tantek Çelik</a>
+    </li>
+    <li class="h-entry">
+      <a href="http://notenoughneon.com/" class="u-feed">Emma Kuo</a>
+    </li>
+  </ul>
+</div>
+EOT
+) ?></pre>
 
 </div>
