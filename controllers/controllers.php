c646a2639b5403dc6d53610ed5153edf08a89ad5
Aaron Parecki <aaron@parecki.com>
2014-06-29 12:56:01 -0700

    add logo and stub doc page

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -28,3 +28,18 @@
 
     });
 
+    $app->get('/docs/?', function($format = 'html') use ($app) {
+
+        $res = $app->response();
+
+        ob_start();
+        render('docs', array(
+            'title'       => 'Docs',
+            'meta'        => '',
+            'authorizing' => false
+        ));
+        $html = ob_get_clean();
+        $res->body($html);
+
+    });
+
---
+++ views/docs.php
@@ -0,0 +1,10 @@
+<div class="narrow">
+
+<?= partial('partials/header') ?>
+
+  <h2>Creating the Subscription List</h2>
+
+
+
+
+</div>
