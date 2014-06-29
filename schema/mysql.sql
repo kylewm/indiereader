c150f8b5930a5c19505992cb87a2b06123813c49
Ben Werdmuller <ben@benwerd.com>
2014-06-29 11:39:36 -0700

    Tables for posts and user state

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -10,3 +10,46 @@ CREATE TABLE IF NOT EXISTS `feed` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `feed_url` (`feed_url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `feed_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `tags` text NOT NULL,
+  `author_name` text NOT NULL,
+  `author_url` text NOT NULL,
+  `author_photo` text NOT NULL,
+  `published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `retrieved` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `timezone_offset` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`),
+  KEY `feed_id` (`feed_id`,`published`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `subscription` (
+  `user_id` int(11) NOT NULL,
+  `feed_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`feed_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_read_post` (
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_replied_post` (
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_bookmarked_post` (
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
